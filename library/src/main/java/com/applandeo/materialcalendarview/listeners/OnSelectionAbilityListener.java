d732630939187d3e1b839e32dba2e5545b16c5ce
Mateusz Kornakiewicz <mateusz.kornakiewicz@applandeo.com>
2017-10-24 12:45:49 +0200

    Added documentation comments

--- library/src/main/java/com/applandeo/materialcalendarview/listeners/OnSelectionAbilityListener.java
+++ library/src/main/java/com/applandeo/materialcalendarview/listeners/OnSelectionAbilityListener.java
@@ -1,6 +1,8 @@
 package com.applandeo.materialcalendarview.listeners;
 
 /**
+ * This interface is used to inform DatePicker that user selected any days
+ *
  * Created by Mateusz Kornakiewicz on 24.10.2017.
  */
 
