d84df7233b0879818a0ca7550e817f97b74ca84e
kormateusz <mathewkor@gmail.com>
2020-04-24 07:54:56 +0200

    Fixed background setting for today

--- sample/src/main/java/com/applandeo/materialcalendarsampleapp/RangePickerActivity.java
+++ sample/src/main/java/com/applandeo/materialcalendarsampleapp/RangePickerActivity.java
@@ -6,9 +6,13 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.applandeo.materialcalendarsampleapp.utils.DrawableUtils;
+import com.applandeo.materialcalendarview.CalendarDay;
 import com.applandeo.materialcalendarview.CalendarView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by Mateusz Kornakiewicz on 23.10.2017.
@@ -22,6 +26,17 @@ public class RangePickerActivity extends AppCompatActivity {
         setContentView(R.layout.range_picker_activity);
 
         CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        CalendarDay calendarDay = new CalendarDay(Calendar.getInstance());
+        calendarDay.setBackgroundDrawable(
+                DrawableUtils.getDayCircle(getApplicationContext(), R.color.defaultColor, android.R.color.transparent));
+        calendarDay.setSelectedBackgroundDrawable(
+                DrawableUtils.getDayCircle(getApplicationContext(), R.color.defaultColor, R.color.sampleLight));
+
+        List<CalendarDay> list = new ArrayList<>();
+        list.add(calendarDay);
+
+        calendarView.setCalendarDays(list);
 
         Button getDateButton = (Button) findViewById(R.id.getDateButton);
         getDateButton.setOnClickListener(v -> {
