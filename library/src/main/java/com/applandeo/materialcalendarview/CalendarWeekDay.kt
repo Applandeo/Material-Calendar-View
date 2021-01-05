dc981021eb6f7d4a0ce826f9a00fbe8432e9361b
kormateusz <mathewkor@gmail.com>
2021-01-05 09:50:47 +0100

    Added ability to set first day of week

---
+++ library/src/main/java/com/applandeo/materialcalendarview/CalendarWeekDay.kt
@@ -0,0 +1,11 @@
+package com.applandeo.materialcalendarview
+
+enum class CalendarWeekDay(val value: Int) {
+    SUNDAY(1),
+    MONDAY(2),
+    TUESDAY(3),
+    WEDNESDAY(4),
+    THURSDAY(5),
+    FRIDAY(6),
+    SATURDAY(7)
+}
