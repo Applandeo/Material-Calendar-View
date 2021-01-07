c025bcd42e1b7acb0df22488ca3e88308f1172a7
kormateusz <mathewkor@gmail.com>
2021-01-07 11:54:23 +0100

    Fixed initial selection in pickers. Removed deprecated method.

--- library/src/main/java/com/applandeo/materialcalendarview/utils/SelectedDay.kt
+++ library/src/main/java/com/applandeo/materialcalendarview/utils/SelectedDay.kt
@@ -20,10 +20,14 @@ data class SelectedDay @JvmOverloads constructor(
 
     override fun equals(other: Any?): Boolean {
         return when (other) {
-            is SelectedDay -> calendar == other.calendar
-            is Calendar -> calendar == other
+            is SelectedDay -> calendar.isTheSameDay(other.calendar)
+            is Calendar -> calendar.isTheSameDay(other)
             else -> super.equals(other)
         }
     }
-}
 
+    private fun Calendar.isTheSameDay(calendar: Calendar): Boolean =
+            this.get(Calendar.DAY_OF_MONTH) == calendar.get(Calendar.DAY_OF_MONTH) &&
+                    this.get(Calendar.MONTH) == calendar.get(Calendar.MONTH) &&
+                    this.get(Calendar.YEAR) == calendar.get(Calendar.YEAR)
+}
